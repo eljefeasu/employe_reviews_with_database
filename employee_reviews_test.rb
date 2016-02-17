c19d852ea001ff4efd417d33c9b51eef8e78b20b
Jefferson Neel <jefferson.neel@gmail.com>
2016-02-17 16:40:18 -0500

    Add test to return the number of employees in a department

--- employee_reviews_test.rb
+++ employee_reviews_test.rb
@@ -144,6 +144,19 @@ class EmployeeReviews < Minitest::Test
     teardown
   end
 
+  def test_number_of_employees_in_department
+    setup
+    a = Department.new(name: "Marketing")
+    xavier = Employee.new(name: "Xavier", email: "ProfX@marvel.com", phone: "911", salary: 70000.00)
+    new_employee = Employee.new(name: "Dan", email: "d@mail.com", phone: "914-555-5555", salary: 50000.00)
+    old_employee = Employee.new(name: "Yvonne", email: "Yvonne@urFired.com", phone: "919-123-4567", salary: 40000.00)
+    a.employees << xavier
+    a.employees << new_employee
+    a.employees << old_employee
+    assert_equal 3, a.employees.length
+    teardown
+  end
+
   private def negative_review_one
     "Zeke is a very positive person and encourages those around him, but he has not done well technically this year. There are two areas in which Zeke has room for improvement. First, when communicating verbally (and sometimes in writing), he has a tendency to use more words than are required. This conversational style does put people at ease, which is valuable, but it often makes the meaning difficult to isolate, and can cause confusion. Second, when discussing new requirements with project managers, less of the information is retained by Zeke long-term than is expected. This has a few negative consequences: 1) time is spent developing features that are not useful and need to be re-run, 2) bugs are introduced in the code and not caught because the tests lack the same information, and 3) clients are told that certain features are complete when they are inadequate. This communication limitation could be the fault of project management, but given that other developers appear to retain more information, this is worth discussing further."
   end
